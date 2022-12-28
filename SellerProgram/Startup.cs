2e606864988ec04f037f70cd7694c8b8a5144787
Lucas Batista <lucas.aps11@gmail.com>
2022-12-28 16:40:37 -0300

    0.0.3

--- SellerProgram/Startup.cs
+++ SellerProgram/Startup.cs
@@ -37,7 +37,8 @@ namespace SellerProgram
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SellerProgramContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SellerProgramContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SellerProgramContext"), 
+                    builder => builder.MigrationsAssembly("SellerProgram")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
