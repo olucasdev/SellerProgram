7f6f552ad36c7b049cad8750633c97e5728acd1c
Lucas Batista <lucas.aps11@gmail.com>
2022-12-28 17:58:34 -0300

    0.0.7

--- SellerProgram/Data/SeedingService.cs
+++ SellerProgram/Data/SeedingService.cs
@@ -74,6 +74,7 @@ namespace SellerProgram.Data
                 r11, r12, r13, r14, r15, r16, r17, r18, r19, r20,
                 r21, r22, r23, r24, r25, r26, r27, r28, r29, r30
             );
+            _context.SaveChanges();
         }
 
     }
--- SellerProgram/Startup.cs
+++ SellerProgram/Startup.cs
@@ -43,11 +43,12 @@ namespace SellerProgram
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
@@ -65,6 +66,8 @@ namespace SellerProgram
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+
         }
     }
 }
