a2af376ffd039fe7fdf5e8ac0344fe74b15ef414
Lucas Batista <lucas.aps11@gmail.com>
2022-12-28 15:06:04 -0300

    0.0.2

---
+++ SellerProgram/Data/SellerProgramContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SellerProgram.Models;
+
+namespace SellerProgram.Data
+{
+    public class SellerProgramContext : DbContext
+    {
+        public SellerProgramContext (DbContextOptions<SellerProgramContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SellerProgram.Models.Department> Department { get; set; }
+    }
+}
--- SellerProgram/Startup.cs
+++ SellerProgram/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SellerProgram.Data;
 
 namespace SellerProgram
 {
@@ -33,6 +35,9 @@ namespace SellerProgram
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SellerProgramContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SellerProgramContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
